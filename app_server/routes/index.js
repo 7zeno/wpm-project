0cdb0079f13b4b63c5f8cc8ec4aeb9f3f05b6c9c
7zeno <217891435+7zeno@users.noreply.github.com>
2025-08-16 17:19:04 +0530

    Changed title

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AIML' });
+  res.render('index', { title: 'Zeno' });
 });
 
 module.exports = router;
